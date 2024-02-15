3ac3c05f9e77c78ae55d77e7631f9e7318782e8d
mmaaddiiiee <madelyn.sutherland2002@gmail.com>
2024-02-14 21:16:01 -0800

    asfjjbsajf

---
+++ Assets/Scripts/RigidbodyMove.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//lines 7-13, 20-28 made using ChatGPT. 
+public class rbmove : MonoBehaviour
+{
+    public float speed = 5f;  // Adjust the speed as needed
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+// used fixed update since dealing with physics objects
+    void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, 0f, vertical) * speed * Time.deltaTime;
+        rb.MovePosition(rb.position + movement);
+    }
+}
+
+
